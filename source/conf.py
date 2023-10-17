428c4651add2792fe0b5a56fe743240900e19c70
Razvan Stoica <razvan.stoica89@gmail.com>
2023-10-17 22:17:29 +0300

    Make docs full width

--- source/conf.py
+++ source/conf.py
@@ -22,7 +22,8 @@ extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.intersphinx",
-    "recommonmark"
+    "recommonmark",
+    "sphinx.ext.githubpages"
 ]
 
 templates_path = ['_templates']
@@ -35,3 +36,9 @@ exclude_patterns = []
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+
+
+def setup(app):
+    app.add_css_file('my_theme.css')
+
